2f0ea2a1436f433ebad53f711ec85c149afb061d
Mashu Ajmera <mashuajmera@gmail.com>
2021-01-26 16:15:51 +0530

    Plotting the curve for polynomial regression using least squares approximation

---
+++ least_squares_regression.m
@@ -0,0 +1,27 @@
+clear
+clc
+close all
+
+max=[57.8 62.2 68.4 76.0 84.3 88.2 90.7 90.7 86.9 79.5 68.0 60.5];
+% max=[6.06 5.56 4.50 5.79 3.28 6.68 6.80 5.19 4.55 3.19 3.15 5.97];
+months=1:length(max);
+
+fig=figure();
+set(fig,'color','white')
+plot(months,max,'b*')
+grid on
+hold on
+xlabel('Month')
+ylabel('Max Temp')
+
+t=max';
+x=months';
+n=3;
+X=[];
+for idx=0:n
+    X=[X,x.^idx];
+end
+
+w=(X'*X)\X'*t;
+y=X*w;
+plot(x,y,'r-')
