914c0d6e0c0cddb06e14e3bafbb9fa4490cabeb3
Mashu Ajmera <mashuajmera@gmail.com>
2021-02-01 14:50:29 +0530

    added comments for easy interpretation

--- mle_regression.m
+++ mle_regression.m
@@ -1,29 +1,33 @@
-clear
-clc
-close all
+clear % clearing the wokspace
+clc % clearing the terminal
+close all % closing all open windows
 
-load accidents
-x = hwydata(:,14);
-t = hwydata(:,4);
-N=length(x);
+load accidents % Importing the dataset
+x = hwydata(:,14); % taking the 14th column as input data
+t = hwydata(:,4); % taking the 4th scolumn as output data
+N=length(x); % taking N as the number of instances
 
-fig=figure();
+fig=figure(); % initializing fig for plot drawing
 set(fig,'color','white')
-plot(x,t,'b*')
-grid on
+plot(x,t,'b*') % plotting input vs output using blue stars
+grid on % configuring to show grid
 hold on
-xlabel('Population')
-ylabel('Accident')
+xlabel('Population') % x axis label
+ylabel('Accident') % y axis label
 
-n=1;
+n=1; % taking complexity of the model
+
+% to compute the matrix for powers of train input column wise
 X=[];
 for idx=0:n
     X=[X,x.^idx];
 end
 
+% using the result from MLE for calculating w and sigma
 w=(X'*X)\X'*t;
 sigma=sqrt((t'*t-t'*X*w)/N);
+
 z=X*w;
-E=normpdf(z,0,sigma);
-y=z+E;
-plot(x,y,'r-')
+E=normpdf(z,0,sigma); % claculating the noise
+y=z+E; % outputting the final value by adding noise to the answer
+plot(x,y,'r-') % plotting the derived curve using red lines
